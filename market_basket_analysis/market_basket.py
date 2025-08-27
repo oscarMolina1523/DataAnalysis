8e70ae6e3666421f4765308e65c1eefb49de8ebe
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-26 19:50:21 -0600

    feat: reading data from csv file

--- market_basket_analysis/market_basket.py
+++ market_basket_analysis/market_basket.py
@@ -1,1 +1,7 @@
-print('Hello World')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+df=pd.read_csv('ventas_ejemplo.csv')
+print(df)
