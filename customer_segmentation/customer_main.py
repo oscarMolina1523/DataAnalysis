bf0b39e3605ed98b9de686e1a591e4770d7958b8
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-29 16:57:07 -0600

    feat: import rfm function in principal file

--- customer_segmentation/customer_main.py
+++ customer_segmentation/customer_main.py
@@ -1,5 +1,7 @@
 from customer_segmentation.customer_preprocessing import preprocessing_data
+from customer_segmentation.rfm_analysis import create_rfm
 
 #This ensures that it only runs when you run it directly and not when you import it.
 if __name__ == "__main__":
     preprocessing_data()
+    create_rfm()
