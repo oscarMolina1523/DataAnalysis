98a7ade61563d90bdead2f195dabd267c323ee86
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-29 13:05:47 -0600

    feat: create a new col named total price and convert dates to datetime

--- customer_segmentation/main.py
+++ customer_segmentation/main.py
@@ -10,5 +10,8 @@ df= df.dropna(subset=['InvoiceNo', 'StockCode', 'Description','Quantity','Invoic
 
 #filter the quatities and unitprice if they are minors 0
 df=df[(df['Quantity']>0) & (df['UnitPrice'] > 0)]
-
+#create a new col named total price
+df['Total Price']=df['Quantity'] * df['UnitPrice']
+#convert dates to datetime
+df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
 print(df)
