c229e313bd84157dfac6db290a83270e9049416e
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-29 10:51:52 -0600

    feat: clean the data to use

--- customer_segmentation/main.py
+++ customer_segmentation/main.py
@@ -2,4 +2,13 @@ import pandas as pd
 
 #First read the csv
 df=pd.read_csv('ventas_ejemplo.csv')
-print(pd.DataFrame(df))
+#print(pd.DataFrame(df))
+
+#clean data
+#here delete all the rows with field empty
+df= df.dropna(subset=['InvoiceNo', 'StockCode', 'Description','Quantity','InvoiceDate', 'UnitPrice', 'CustomerID', 'Country'])
+
+#filter the quatities and unitprice if they are minors 0
+df=df[(df['Quantity']>0) & (df['UnitPrice'] > 0)]
+
+print(df)
