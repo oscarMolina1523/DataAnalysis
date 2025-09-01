34ed2935f9374c730c06ce3cf453ff4338df361d
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-31 19:01:36 -0600

    feat: create charts to visualice the info

---
+++ customer_segmentation/visualization.py
@@ -0,0 +1,17 @@
+# visualization.py
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_clusters_scatter(rfm):
+    plt.scatter(rfm["Recency"], rfm["Monetary"], c=rfm["Cluster"], cmap="viridis")
+    plt.xlabel("Recency")
+    plt.ylabel("Monetary")
+    plt.title("Clusters de clientes (Recency vs Monetary)")
+    plt.show()
+
+def plot_cluster_profiles(cluster_profile):
+    cluster_profile.plot(kind="bar")
+    plt.title("Perfil promedio de cada cluster")
+    plt.xlabel("Cluster")
+    plt.ylabel("Valores RFM")
+    plt.show()
