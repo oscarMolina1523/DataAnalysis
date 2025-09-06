a17cdec23f93c1a5a7195ac6baea4447d36def43
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-06 07:23:42 -0600

    docs: add link to notion page to understand the code

--- market_basket_analysis/market_main.py
+++ market_basket_analysis/market_main.py
@@ -1,3 +1,9 @@
+#If you want read how does this code works
+#i create a notion page to explain it
+#in a simple way
+#link: https://www.notion.so/mineria-de-datos/Market-Basket-Analysis-an-lisis-de-la-cesta-de-la-compra-2647c9c9fb1780cf8730dbc85cc814f9?source=copy_link
+
+
 from market_basket_analysis.market_preprocessing import preprocess_data
 from market_basket_analysis.market_transactions import create_transaction_matrix
 from market_basket_analysis.apriori_analysis import run_apriori
