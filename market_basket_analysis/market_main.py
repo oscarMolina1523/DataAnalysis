25835a81e9a5c6dc245c72cec3a49e64479cf11f
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-06 07:14:21 -0600

    docs: clean comments in market_main file

--- market_basket_analysis/market_main.py
+++ market_basket_analysis/market_main.py
@@ -5,13 +5,6 @@ from market_basket_analysis.market_visualization import plot_top_rules, plot_sca
 
 #This ensures that it only runs when you run it directly and not when you import it.
 if __name__ == "__main__":
-    #rules[['antecedents','consequents','support','confidence','lift']].head(10)
-    #top_rules = rules.sort_values(by="support", ascending=False).head(10)
-
-    #this is a red of rules showing in console TABLE
-    # top_rules_display = top_rules.copy()
-    # top_rules_display['antecedents'] = top_rules_display['antecedents'].apply(fs_to_str)
-    # top_rules_display['consequents'] = top_rules_display['consequents'].apply(fs_to_str)
 
     # 1. preprocessing
     preprocess_data()
